b4c8eef8955bc30caa7070560df89f69b7bf95c5
Tetiana Korchak <tkorchak@mirantisits-MacBook-Pro-5.local>
2017-03-01 16:46:12 -0800

    refactored combine_files.py; added help

--- combine_files.py
+++ combine_files.py
@@ -1,39 +1,39 @@
-"""
-python combine_files.py cinder/ cinder_scenarios.yaml
-1. deletes file cinder_scenarios.yaml if it exists
-2. collects all the yaml files to the list
-3. writes data from all files to temp.file
-4. removes lines with '---' except the first one and writes it to sys.argv[2] file
-5. deletes temp.file file
+#!/usr/bin/python
 
-"""
-
-import sys
+import argparse
 import glob
 import os
 
-path = sys.argv[1]
-file_name = sys.argv[2]
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', help='path to .yaml files, if specified then scenarios will be combined '
+                                   'only for the specific component (e.g. nova)')
+parser.add_argument('--filename', help='name of a new file where all scenarios will be stored')
 
+args = parser.parse_args()
+path = args.path
+file_name = args.filename
+
+# remove file if exists
 if os.path.isfile(file_name):
     print(os.path.isfile(file_name))
     os.system('rm ' + file_name)
 
-files = glob.glob(path + '*.yaml')
+# check path
+if path is not None:
+    files = glob.glob(path + '*.yaml')
+else:
+    files = glob.glob("*/" + '*.yaml')
 
+# write scenarios to a temporary file
 temp_file = open('temp.file', 'w')
-
 for afile in files:
-    if 'task_arguments' in afile:
-        continue
-    else:
-        data = open(afile).read()
-        temp_file.write(data)
-        temp_file.write('\n\n')
+    data = open(afile).read()
+    temp_file.write(data)
+    temp_file.write('\n\n')
 temp_file.close()
 
+# rewrite data to another file and remove lines with '---'
 all_yaml_scenarios_file = open(file_name, 'w')
-
 if os.access('temp.file', os.R_OK):
     with open('temp.file', 'r') as fn:
         # to leave the first '---' from deleting
@@ -43,17 +43,5 @@ if os.access('temp.file', os.R_OK):
             if '---' not in line:
                 all_yaml_scenarios_file.write(line)
 
-fn.close()
 all_yaml_scenarios_file.close()
 os.system('rm temp.file')
-
-
-if file_name == 'heat_scenarios.yaml':
-    file_data = open(file_name).read()
-    with open(file_name, 'w') as fn:
-        fn.write('{% set image_name = image_name or "^cirros.*-disk$" %}\n')
-        fn.write('{% set public_net = neutron_floating_network or "public" %}\n')
-        fn.write(file_data)
-    fn.close()
-
-
